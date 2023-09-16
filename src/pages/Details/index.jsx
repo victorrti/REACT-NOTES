0ecc746775cf33d4b15d5aef4e9d254cd46e8864
victor <victorrodriguesti@gmail.com>
2023-09-16 13:46:24 -0300

    finalizando tela de rolagem das notas

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -6,25 +6,28 @@ import {Tag} from '../../components/Tags'
 import {ButtonText} from '../../components/ButtonText'
 export  function Details(){
   return(
-    <main>
-      <Content>
-        <Container>
-          <Header></Header>
-          <ButtonText title="excluir Nota"></ButtonText>
-            <Section title="Links uteis">
-              <Link>
-                  <li><a href="https://github.com/victorrti">victor rodrigues github</a></li>          
-              </Link>
-              </Section>
-              <Section title="Marcadores">
-                <Tag title="React"></Tag>
-                <Tag title="Node"></Tag>
-              </Section>
-              <Button title="Voltar" loading={true} />
-        </Container>                
+    
+    <Container>
+      <Header></Header>
+      <main>
+        <Content>       
+        <ButtonText title="excluir Nota"></ButtonText>
+        <h1>Introdução React</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Ducimus molestias architecto quia corporis consequuntur fugit tempora possimus est, maxime voluptatem, a qui beatae laudantium aliquam provident nesciunt ab illo aut.</p>
+        <Section title="Links uteis">
+          <Link>
+            <li><a href="https://github.com/victorrti">victor rodrigues github</a></li>          
+          </Link>
+        </Section>
+        <Section title="Marcadores">
+          <Tag title="React"></Tag>
+          <Tag title="Node"></Tag>
+        </Section>
+        <Button title="Voltar" loading={true} />
       </Content>
-    </main>
-   
+      </main>
+    </Container>
      
   )
 }
