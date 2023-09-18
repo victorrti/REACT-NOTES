ee47869e78b58dc90cf9dbd3f5319cf279a33656
victor <victorrodriguesti@gmail.com>
2023-09-17 23:13:16 -0300

    incluindo tela de cadastro

---
+++ src/pages/SingUp/index.jsx
@@ -0,0 +1,36 @@
+import {Container,Form,Background} from './style'
+import {Input} from '../../components/Input'
+import {FiLogIn,FiMail, FiLock,FiUser} from 'react-icons/fi'
+import {Button} from '../../components/Button'
+import {ButtonText} from '../../components/ButtonText'
+
+
+export function SingUp(){
+    return(
+        <Container>
+            <Background/>
+            <Form>
+                <h1>Rocket Notes</h1>
+                <p>Aplicação para gerenciar e salvar seus links Uteis.</p>
+                <h2>Crie Sua Conta</h2>
+                <Input 
+                placeholder="Nome"
+                type="text"
+                icon={FiUser}/>
+                <Input 
+                placeholder="E-mail"
+                type="text"
+                icon={FiMail}/>
+                <Input 
+                placeholder="Senha"
+                type="password"
+                icon={FiLock}/>
+                <Button title="cadastrar"/>
+                <ButtonText title="Voltar Parar o Login"/>
+                
+            </Form>
+            
+        </Container>
+    )
+
+}
